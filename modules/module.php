f1bbb5a988025dd40ba70b5b8787e83fdd160479
johncarlson21@gmail.com <johncarlson21@gmail.com@ad79945b-51b3-c64c-30f8-a022932c01a8>
2012-01-24 20:14:58 +0000

    update module.php
    git-svn-id: https://etofork.googlecode.com/svn@74 ad79945b-51b3-c64c-30f8-a022932c01a8

--- modules/module.php
+++ modules/module.php
@@ -71,6 +71,91 @@ class module {
             require_once (absolute_base_path . 'modules/' . $module . "/" . $module . "_admin.php");
         }
     }
+    
+    public function listModules() {
+        
+    }
+    
+    public function installModule() {
+        // form to ask for the module name basically the first part of the .tgz file
+        include_once(MANAGER_PATH . 'views/install_module.phtml');
+    }
+    
+    public function runModuleInstall() {
+        $this->validateRequest(array('module'));
+        $module = $_REQUEST['module'];
+        // first try to extract the module
+        include_once(MANAGER_PATH . 'lib/easyarchives/EasyArchive.class.php');
+        $arch = new archive;
+        if (!$arch->extract(absolute_base_path . 'tmp/packages/'.$module.'.tar', absolute_base_path . 'tmp/packages/')) {
+            $this->respond(false, 'There was an error extracting the module!');
+        }
+        // run the install of the file (moving them and running the creation of sections of snippets)
+        $Resource = new Resource();
+        if (file_exists(absolute_base_path . 'tmp/packages/'.$module.'/install.php')) {
+            include_once(absolute_base_path . 'tmp/packages/'.$module.'/install.php');
+            $snSectionId = 1; // set to default section
+            $chSectionId = 2; // set to default section
+            if (isset($module_name) && isset($resources)) {
+                // don't create sections unless we have resources for them
+                // create new sections for snippets and chunks
+                // create new snippets and chunks
+                if (isset($resources['snippets']) && count($resources['snippets']) > 0){
+                    if (!$Resource->sectionExists($module_name, 'snippet')) {
+                        $Resource->createSection($module_name, 'Module: '.$module_name.' snippet section', 'snippet');
+                        $snSectionId = $Resource->insertId();
+                    } else {
+                        $section = $Resource->getSection($module_name, 'snippet');
+                        $snSectionId = $section['id'];
+                    }
+                    // create any snippets
+                    foreach($resources['snippets'] as $snippet) {
+                        // check for file first
+                        if (file_exists(absolute_base_path . 'tmp/packages/'.$module.'/resources/snippets/'.$snippet['ref'].'.php')) {
+                            $snCode = @file_get_contents(absolute_base_path . 'tmp/packages/'.$module.'/resources/snippets/'.$snippet['ref'].'.php');
+                            $result = $Resource->putIntTableRow(array(
+                                'name'=>$snippet['name'],
+                                'description'=>$snippet['description'],
+                                'snippet'=>$snCode,
+                                'section'=>$snSectionId
+                            ), 'site_snippets');
+                        }
+                    }
+                }// end if for snippets
+                if (isset($resources['chunks']) && count($resources['chunks']) > 0){
+                    if (!$Resource->sectionExists($module_name, 'chunk')) {
+                        $Resource->createSection($module_name, 'Module: '.$module_name.' chunk section', 'chunk');
+                        $chSectionId = $Resource->insertId();
+                    } else {
+                        $section = $Resource->getSection($module_name, 'chunk');
+                        $chSectionId = $section['id'];
+                    }
+                    foreach($resources['chunks'] as $chunk) {
+                        // check for file first
+                        if (file_exists(absolute_base_path . 'tmp/packages/'.$module.'/resources/chunks/'.$chunk['ref'].'.php')) {
+                            $chCode = @file_get_contents(absolute_base_path . 'tmp/packages/'.$module.'/resources/chunks/'.$chunk['ref'].'.php');
+                            $result = $Resource->putIntTableRow(array(
+                                'name'=>$chunk['name'],
+                                'description'=>$chunk['description'],
+                                'snippet'=>$chCode,
+                                'section'=>$chSectionId
+                            ), 'site_htmlsnippets');
+                        }
+                    }
+                }// end if for chunks
+            }// end check for module name and resources
+            
+        } // end if for install script
+        
+        // move folder to modules folder
+        $Resource->rcopy(absolute_base_path . 'tmp/packages/' . $module . "/", absolute_base_path . 'modules/' . $module);
+        // remove tmp module directory
+        $Resource->rrmdir(absolute_base_path . 'tmp/packages/' . $module . "/");
+        // remove package file
+        @unlink(absolute_base_path . 'tmp/packages/' . $module . ".tar");
+        
+        $this->respond(true, 'Module Installed!');
+    }
 
 }
 
