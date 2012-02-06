c1a116fabf92c24b88d9acd95fae96a7fdb70c81
johncarlson21@gmail.com <johncarlson21@gmail.com@ad79945b-51b3-c64c-30f8-a022932c01a8>
2012-02-06 20:24:10 +0000

    version update and more html changes
    git-svn-id: https://etofork.googlecode.com/svn@125 ad79945b-51b3-c64c-30f8-a022932c01a8

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -3,11 +3,12 @@
 // Modified: 2008-04-18 [v1.0] by Ralph A. Dahlgren
 // Modified: 2008-05-08 [v1.1] by Ralph A. Dahlgren
 // Modified: 2012-01-02 [v2.0] by John M. Carlson
-// current revision: 123
+// current revision: 125
+$svn_rev = 125;
 $small_version = "2"; // Current codebase
 $patch_level = ".2"; // Revision number
-$release = "2.2"; // Full current Etomite release number
+$release = "2.2 ".$svn_rev; // Full current Etomite release number
 $code_name = "Raven Alpha"; // Current Etomite codename
-$full_appname = "EtoFork - Etomite CMS ".$small_version.$patch_level." ($code_name)";
+$full_appname = "EtoFork - Etomite CMS ".$release." ($code_name)";
 $full_slogan = "Fast, Free, Infinitely Flexible";
 ?>
