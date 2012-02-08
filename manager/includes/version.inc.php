19f83b545f19863efe4a88e5cb5511b2bb4b653e
johncarlson21@gmail.com <johncarlson21@gmail.com@ad79945b-51b3-c64c-30f8-a022932c01a8>
2012-02-08 19:23:28 +0000

    revision change in version file
    git-svn-id: https://etofork.googlecode.com/svn@134 ad79945b-51b3-c64c-30f8-a022932c01a8

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -4,7 +4,7 @@
 // Modified: 2008-05-08 [v1.1] by Ralph A. Dahlgren
 // Modified: 2012-01-02 [v2.0] by John M. Carlson
 // current revision: 125
-$svn_rev = 125;
+$svn_rev = 134;
 $small_version = "2"; // Current codebase
 $patch_level = ".2"; // Revision number
 $release = "2.2 r".$svn_rev; // Full current Etomite release number
